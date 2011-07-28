4fc44b3d7de1b2649a8cdd1e2e8e4c979354781b
Chang Wang <changwang83@gmail.com>
2011-07-28 16:29:47 -0400

    added template layout and a dummy view

--- urls.py
+++ urls.py
@@ -2,9 +2,14 @@ from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # votes
+    url(r'^$', include('votes.urls')),
+    
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
 )
